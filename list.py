7ac2f824bb65414cf87cb542ae3b96a42c8e0336
Princepratapsingh <prince.singh_bca22@gla.ac.in>
2024-01-06 14:20:32 +0530

    Initial Commit

---
+++ list.py
@@ -0,0 +1,9 @@
+# n=int(input("Enter the Size"))
+# s=(input("Enter the Elements"))
+# list=list(map(int,s.split()))[:n]
+# print(list)
+m=int(input())
+list=input()
+M=list.split()
+f=list(map(int,M))
+print(f)
